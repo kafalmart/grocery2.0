c12bc55998116e7208d315354ce6bcdbdb4a1659
kafalmart <order.kafalmart@gmail.com>
2026-06-30 05:35:42 +0530

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,25 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import ClientLayout from "../components/layout/ClientLayout";
+
+export const metadata: Metadata = {
+  title: "KafalMart",
+  description:
+    "Good Food, Great Mood. Delicious food delivered to your doorstep.",
+
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+      },
+      {
+        url: "/favicon-48x48.png",
+        sizes: "48x48",
+        type: "image/png",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
