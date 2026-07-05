d319230f2d590b4fa1bb22e7a3f97e10b20c36a5
kafalmart <order.kafalmart@gmail.com>
2026-07-05 17:19:39 +0530

    Uncomment database connection in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 import "./config/env.js";
 import app from "./app.js";
-// import connectDB from "./config/db.js";
+ import connectDB from "./config/db.js";
 
-// connectDB();
+ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
