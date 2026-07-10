6d7f570dc030581446718412d4cf9e22802e924d
kafalmart <order.kafalmart@gmail.com>
2026-07-10 12:38:03 +0530

    Refactor server.js to integrate socket management
    Refactor server setup to use setIO for socket management.

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,16 +5,20 @@ import connectDB from "./config/db.js";
 import http from "http";
 import { Server } from "socket.io";
 
+import { setIO } from "./socket.js";
+
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-export const io = new Server(server, {
+const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
+
+setIO(io);
 
 io.on("connection", (socket) => {
   console.log("🟢 Partner Connected:", socket.id);
@@ -31,8 +35,8 @@ const startServer = async () => {
     server.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
+
   } catch (error) {
-    console.error("❌ Failed to start server");
     console.error(error);
     process.exit(1);
   }
